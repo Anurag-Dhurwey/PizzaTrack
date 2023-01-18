c3ff00d6cda970a3c59aeb8c28b87fd3b6673ed4
Anurag dhurwey <anuragdhurwey9211@gmail.com>
2023-01-18 17:02:49 +0530

    app is more secured

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,3 @@
 let mix = require('laravel-mix');
-
+mix.disableSuccessNotifications();
 mix.js('resources/js/app.js', './public/script/script.js').sass('resources/sass/app.scss', './public/css/style.css');
